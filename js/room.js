6c8aa929305d8eb5b9713723a3a3d98c67c29a3d
Damian Wrather <damian.wrather@gmail.com>
2023-08-16 13:29:11 -0500

    js

---
+++ js/room.js
@@ -0,0 +1,9 @@
+let leftArrow = document.querySelector('.left-arrow');
+let rightArrow = document.querySelector('.right-arrow');
+
+leftArrow.addEventListener('click', ()=>{
+
+})
+rightArrow.addEventListener('click',()=>{
+
+})
